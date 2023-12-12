c318941de9fd570297643e3220cad3824ea576d0
Ovidiu Latcu <ovidiu.latcu@gmail.com>
2023-12-13 00:42:24 +0200

    Day 9 part 2

---
+++ src/day9-2.ts
@@ -0,0 +1,27 @@
+import * as fs from "fs";
+
+const content = fs
+  .readFileSync("assets/input9.txt", "utf-8")
+  .split("\n")
+  .filter((line) => line.length > 0);
+
+function allZeroes(arr: number[]) {
+  return arr.every((it) => it === 0);
+}
+
+function calculatePrediction(data: number[]) {
+  if (allZeroes(data)) {
+    return 0;
+  }
+  let diffs: number[] = [];
+  for (let i = 0; i < data.length - 1; i++) {
+    diffs.push(data[i + 1] - data[i]);
+  }
+  return data[0] - calculatePrediction(diffs);
+}
+
+const lineToNums = (line: string) => line.split(" ").map((i) => +i);
+
+const sum = content.reduce((prev, curr) => prev + calculatePrediction(lineToNums(curr)), 0);
+
+console.log("SUM=", sum);
